b450335e6ed0e3e4ec044534737cf99931cd6a8b
oatThanut <oat_thanut@hotmail.com>
2017-12-20 21:16:57 +0700

    Initial Commit

---
+++ CryptocurrencyTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptocurrencyTracker
+//
+//  Created by oatThanut on 20/12/2560 BE.
+//  Copyright © 2560 WTFart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
