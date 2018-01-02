12932b054988952f3be591f6224445bd7b9d23c4
oatThanut <thanut@Thanut-MacBook.local>
2018-01-02 21:25:07 +0700

    Create HttpManager

---
+++ CTHttpManager.swift
@@ -0,0 +1,15 @@
+//
+//  VTHttpManager.swift
+//  CryptocurrencyTracker
+//
+//  Created by oatThanut on 2/1/2561 BE.
+//  Copyright © 2561 WTFart. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class CTHttpManager {
+    static let shareInstance = CTHttpManager();
+    
+}
